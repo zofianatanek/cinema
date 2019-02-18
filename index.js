fa439318348a8faa91b666b2ab9576a78c238b77
zofianatanek <zofia.natanek@gmail.com>
2019-02-18 20:11:11 +0100

    email and password verficication

--- index.js
+++ index.js
@@ -2,18 +2,33 @@
 // function that verifies correctness of an email
 function check_email(){
     var login = document.getElementById("email").value;
+    var spr = new RegExp('[a-z1-9]{2,}[@][a-z1-9]{2,}[.][a-z1-9]{2,3}')
+    if((spr.test(login))){
+        console.log("poprawny email");
+    }
+    else {
+        alert("niepoprawny email!");
+    }
 }
 
 
 // function that verifies correctness of an password
 function check_password(){
-    var login = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var spr = new RegExp('[a-z1-9]{6,}')
+    if((spr.test(password))){
+        console.log("haslo ok");
+    }
+    else {
+        alert("haslo za krotkie!");
+    }
 }
 
 // function that checks if email is in base
 function log_in(){
     var login = document.getElementById("email").value;
     var email = login.toLowerCase();
+    alert(email);
 
 }
 
