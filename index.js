16013cc39b27c677ccfaed0812fdddd5d7d3bd36
zofianatanek <zofia.natanek@gmail.com>
2019-02-18 19:15:48 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// function that verifies correctness of an email
+function check_email(){
+    var login = document.getElementById("email").value;
+}
+
+
+// function that verifies correctness of an password
+function check_password(){
+    var login = document.getElementById("email").value;
+}
+
+// function that checks if email is in base
+function log_in(){
+    var login = document.getElementById("email").value;
+    var email = login.toLowerCase();
+
+}
+
