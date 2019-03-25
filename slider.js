972b41492e1ae08ce8c8af834617ca1fb1ca86a6
zofianatanek <zofia.natanek@gmail.com>
2019-03-25 19:28:46 +0100

    slider added to the home page

--- slider.js
+++ slider.js
@@ -1,4 +1,17 @@
-var slideIndex = 0;
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+
 showSlides();
 
 function showSlides() {
@@ -10,5 +23,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1} 
   slides[slideIndex-1].style.display = "block"; 
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
 }
